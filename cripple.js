4fa1fb0193d44304292771edbb8194957ba86ac3
Fábio Santos <fabiosantosart@gmail.com>
2014-12-29 20:19:49 +0000

    random improvements, removed IE6 from the mix

--- cripple.js
+++ cripple.js
@@ -19,45 +19,80 @@
     };
 
     globalObj.cripple = function (tags) {
-        var es = find('es1', tags) ? 1:
-                 find('es3', tags) ? 3:
-                 find('es4', tags) ? 4:  // ???
+        var es = find('es3', tags) ? 3:
                  find('es5', tags) ? 5:
                  find('es6', tags) ? 6:
                  undefined;
-        var ie = find('ie6', tags) ? 6:
-                 find('ie7', tags) ? 7:
+        var ie = find('ie7', tags) ? 7:
                  find('ie8', tags) ? 8:
                  find('ie9', tags) ? 9:
+                 find('ie10', tags) ? 10:
+                 find('ie11', tags) ? 11:
                  undefined;
-        
+
+        if (es === undefined && ie !== undefined) {
+            es = ie < 8 ?
+                3 :  // ES3 in IE7
+                5;   // ES5 in IE8
+        }
+
+        if (es === undefined && ie === undefined) {
+            throw new Error('"tags" should include esX or ieY, X being 3, 5, or 6, and Y being between 7 and 11');
+        }
+
         // Event Handling
         (function () {
             var elmProto = Element.prototype
-            if (ie && ie < 8) {
+            if (ie && ie < 9) {
                 var ael = elmProto.addEventListener;
                 var rel = elmProto.removeEventListener;
                 var events = [];
-                
+
                 elmProto.attachEvent = function (evt, cb) {
-                    var evtname = [].slice.call(evt, 2, Infinity);  // remove the "on" part
-                    events.push(cb);
-                    ael.call(this, evtname, function (e) {
+                    events.push({ evt: evt, cb: cb });
+                    ael.call(this, evt.replace(/^on/, ''), function (e) {
                         window.event = e;
-                        cb.call(this);  // TODO figure out what to do with return false
+                        try {
+                            var ret = cb.call(this);
+                        } catch(e) {
+                            console.error(e);
+                        }
                         window.event = null;
+                        return ret;
                     }, false);
                 };
 
                 elmProto.detachEvent = function (evt, func) {
-                    var evtname = [].slice.call(evt, 2, Infinity);  // remove the "on" part
-                    for (var i = 0, len = events.length; i < len; i++) if (events === func) {
-                        break;
+                    for (var i = 0, len = events.length; i < len; i++) {
+                        if (events[i].cb === func && events[i].evt === evt) {
+                            break;
+                        }
                     }
-                    rel.call(this, evtname, 
+                    if (events[i].cb !== func || events[i].evt !== evt) { return; }
+                    rel.call(this, events[i].evt.replace(/^on/, ''), events[i].cb);
                 }
 
                 disable(elmProto, 'addEventListener');
+                disable(elmProto, 'removeEventListener');
+            }
+            // TODO event button numbers
+            // TODO can't fire some events on hidden elements
+            // TODO gold mine of bugs for each IE version with javascript.
+        }());
+
+        // Element stuff
+        (function () {
+            var elmProto = Element.prototype
+
+            // Remove "height" and "width" (and other) getters from rects. These are chrome/firefox specific
+            for (var rectProp in DOMRectReadOnly.prototype) {
+                if (['top', 'bottom', 'left', 'right'].indexOf(rectProp) < 0) {
+                    disable(DOMRectReadOnly.prototype, rectProp);
+                }
+            }
+
+            // TODO when did this come along? disable(elmProto, 'classList');
+            if (ie && ie < 8) {
                 disable(elmProto, 'removeEventListener');
             }
             // TODO gold mine of bugs for each IE version with javascript.
@@ -66,17 +101,6 @@
         // String prototype
         (function () {
             var stringProto = String.prototype;
-            if (es && es < 3) {
-                disable(stringProto, 'replace');
-                disable(stringProto, 'concat');
-                disable(stringProto, 'search');
-                disable(stringProto, 'split');
-                disable(stringProto, 'slice');
-                disable(stringProto, 'match');
-                disable(stringProto, 'localeCompare');
-                disable(stringProto, 'toLocaleLowerCase');
-                disable(stringProto, 'toLocaleUpperCase');
-            }
             if (es && es < 5) {
                 disable(stringProto, 'trim');
             }
@@ -91,15 +115,6 @@
         (function () {
             var arrayProto = Array.prototype;
             // TODO Array#sort has a few quirks
-            if (es && es < 3) {
-                disable(arrayProto, 'pop');
-                disable(arrayProto, 'push');
-                disable(arrayProto, 'shift');
-                disable(arrayProto, 'unshift');
-                disable(arrayProto, 'splice');
-                disable(arrayProto, 'concat');
-                disable(arrayProto, 'slice');
-            }
             if (es && es < 5) {
                 // Really useful stuff:
                 disable(arrayProto, 'indexOf');
@@ -118,23 +133,15 @@
         // Function prototype
         (function () {
             var functionProto = Function.prototype;
-            if (es && es < 3) {
-                disable(arrayProto, 'apply');
-                disable(arrayProto, 'call');
-            }
             if (es && es < 5) {
                 // Really useful stuff:
-                disable(arrayProto, 'bind');
+                disable(functionProto, 'bind');
             }
         }());
         
         // RegExp prototype
         (function () {
             var regexpProto = RegExp.prototype;
-            if (es && es < 3) {
-                disable(regexpProto, 'exec');
-                disable(regexpProto, 'test');
-            }
         }());
 
         // Object prototype
@@ -190,5 +197,7 @@
             // TODO Date.toDateString docs were inconclusive about when it came up
         }());
 
+        // TODO getComputedStyle
+        // TODO focus()ing elements which are hidden throws an error
     };
 }(window));
